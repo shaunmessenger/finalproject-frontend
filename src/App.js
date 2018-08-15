caa428d857828e1795058c749aecfcaa3f6bb921
Your Name <you@example.com>
2018-08-15 15:19:52 -0400

    variableExpense, App, and GoalSetup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-import FixedInputs from './components/FixedInputs'
-=======
 import Login from './components/Login';
 import Signup from './components/Signup.js';
 import GoalSetup from './components/GoalSetup.js';
@@ -13,7 +10,6 @@ import VariableExpense from './components/VariableExpense';
 import SavingsStatus from './components/SavingsStatus';
 import DailyBudget from './components/DailyBudget';
 import EndOfDay from './components/EndOfDay';
->>>>>>> 68e9f554acaa2d3517e3540f14a6ebf51f0c1cdd
 
 class App extends Component {
   constructor() {
@@ -24,29 +20,17 @@ class App extends Component {
     this.getUserIdFromLogin = this.getUserIdFromLogin.bind(this)
   }
   getUserIdFromLogin (userID)  {
-    this.setState({userID: userID})
+    this.setState({userID: userID})//this code is sending the userID to the components where needed
   }
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <FixedInputs/>
-      </div>
-=======
       <BrowserRouter>
         <div>
     <Route path = '/' exact ={true} render ={() => <Login sendUserIDToApp ={this.getUserIdFromLogin}/>} />
         <Route path = '/Signup' exact ={true} render ={() => <Signup/>} />
-        <Route path = '/setUpGoal' exact ={true} render ={() => <GoalSetup userID= {this.state.userID}/>} />
+        <Route path = '/setUpGoal' exact ={true} render ={() => <GoalSetup userID = {this.state.userID}/>} />
         <Route path = '/setUpFixed' exact ={true} render ={() => <FixedSetup/>} />
-        <Route path = '/inputVariable' exact ={true} render ={() => <VariableExpense/>} />
+        <Route path = '/inputVariable' exact ={true} render ={() => <VariableExpense userID = {this.state.userID}/>} />
         <Route path = '/getSavingsStatus' exact ={true} render ={() => <SavingsStatus/>} />
         <Route path = '/todaysBudget' exact ={true} render ={() => <DailyBudget/>} />
         <Route path = '/endOfDay' exact ={true} render ={() => <EndOfDay/>} />
@@ -54,7 +38,6 @@ class App extends Component {
         </div>      
       </BrowserRouter>  
       
->>>>>>> 68e9f554acaa2d3517e3540f14a6ebf51f0c1cdd
     );
   }
 }
