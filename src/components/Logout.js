94bb36433446a793fa614e36b425765d92a0bcb1
Lisa Vineberg <lisavineberg@Lisas-MacBook-Air.local>
2018-08-17 11:29:40 -0400

    L: started logout component

---
+++ src/components/Logout.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class LogoutBasic extends Component {
+
+    
+}
+
+let Logout = withRouter(LogoutBasic)
+export default Logout
