dcbf3619355da1a3ee0a25eb0b2a85d951db50bf
Lisa Vineberg <lisavineberg@Lisas-MacBook-Air.local>
2018-08-20 18:40:54 -0400

    L: more css, endofday works but not updated

--- src/components/EndOfDay.js
+++ src/components/EndOfDay.js
@@ -1,14 +1,17 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import AlertDialogSlide from "./PopUp";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import AlertDialogSlide from './PopUp';
+
+
+
 
 class EndOfDayForm extends Component {
     constructor() {
         super();
         this.state = {
             daySavings: null,
-            dayRollover:null,
-            open: false
+            dayRollover: null,
+            open: false,
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleSavings = this.handleSavings.bind(this)
@@ -18,7 +21,6 @@ class EndOfDayForm extends Component {
 
     handleSubmit(evt) {
         evt.preventDefault();
-        if(this.state.daySavings && this.state.dayRollover){
         this.setState({open: !this.state.open})
         let bod;
         if (this.props.todaysBudget > 0) {
@@ -49,50 +51,13 @@ class EndOfDayForm extends Component {
             this.props.sendInfoToApp(todaysBudget, todaysVariable)
             
         })
-    }    
     }
 
     handleSavings(evt) {
         this.setState({ daySavings: evt.target.value })
     }
-    console.log(bod);
-    fetch("/endOfDay", {
-      method: "POST",
-      body: bod
-    })
-      .then(response => response.text())
-      .then(response => {
-        let parsed = JSON.parse(response);
-        console.log(parsed);
-        let todaysBudget = parsed.todaysBudget;
-        let todaysVariable = parsed.todaysVariable;
-        this.props.sendInfoToApp(todaysBudget, todaysVariable);
-      });
-  }
-
-  handleSavings(evt) {
-    this.setState({ daySavings: evt.target.value });
-  }
-  handleRollover(evt) {
-    this.setState({ dayRollover: evt.target.value });
-  }
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-  handleClose = () => {
-    this.setState({ open: false });
-    this.props.history.push("/getSavingsStatus");
-  };
-
-  render() {
-    let text;
-    if (this.state.daySavings > this.props.dailySaveGoal) {
-      text = "You surpassed your daily save goal! Congratulations!";
-    } else if (this.state.daySavings < this.props.dailySaveGoal) {
-      text =
-        "You didn't reach your goal, but you still managed to save something!";
-    } else {
-      text = "You hit your daily save goal for the day! Keep it up!";
+    handleRollover(evt) {
+        this.setState({ dayRollover: evt.target.value })
     }
     handleClickOpen = () => {
         this.setState({ open: true });
@@ -122,15 +87,14 @@ class EndOfDayForm extends Component {
                         <form onSubmit={this.handleSubmit}>
                             <h2>Done for the day?</h2>
                             Add to savings:
-                <input placeholder={'Your savings goal is: $' + this.props.dailySaveGoal}
+                <input placeholder={'$' + this.props.dailySaveGoal}
                                 value={this.state.daySavings}
                                 onChange={this.handleSavings} />
-                                <br/>
                             Rollover to tomorrow:
                 <input placeholder={'$' + (this.props.todaysBudget - this.state.daySavings)}
                                 value={this.state.dayRollover}
                                 onChange={this.handleRollover} />
-                            <div>${this.props.dailySaveGoal}</div>
+                            <div>Your savings goal for today: ${this.props.dailySaveGoal}</div>
                             <div>Budget remaining: ${this.props.todaysBudget}</div>
                             <input type="submit" />
                         </form>
@@ -138,38 +102,22 @@ class EndOfDayForm extends Component {
 
 
                     <div>
-                        <form onSubmit={this.handleSubmit}>you over spent today, the balance has been deducted from tomorrow's budget
-                <input type='submit' value="go back"/>
+                        <form onSubmit={this.handleSubmit}>you over spent today
+                        <input type='submit' value="go back"/>
                         </form>
                     </div>
 
 
                 }
-                value={this.state.dayRollover}
-                onChange={this.handleRollover}
-              />
-              <div>
-                Your savings goal for today: ${this.props.dailySaveGoal}
-              </div>
-              <div>Budget remaining: ${this.props.todaysBudget}</div>
-              <input type="submit" />
-            </form>
-          </div>
-        ) : (
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              you over spent today, the balance has been deducted from
-              tomorrow's budget
-              <input type="submit" value="go back" />
-            </form>
-          </div>
-        )}
-      </div>
-    );
-  }
+
+
+            </div>
+        )
+
+
+    }
 }
 
-let EndOfDay = withRouter(EndOfDayForm);
+let EndOfDay = withRouter(EndOfDayForm)
 export default EndOfDay;
 
-//jainal 16/8/2018
--- src/components/GoalSetup.js
+++ src/components/GoalSetup.js
@@ -9,6 +9,7 @@ import Button from "@material-ui/core/Button";
 import InputGoalAmount from "./MaterialUI/GoalsInputAmount";
 import InputGoalDate from "./MaterialUI/GoalsInputDate";
 import GoalsSubmitButton from "./MaterialUI/GoalsSubmitButton";
+import UnrealisticGoalSubmitButton from "./MaterialUI/UnrealisticGoalSubmitButton"
 
 class GoalSetupForm extends Component {
   constructor() {
@@ -79,9 +80,9 @@ class GoalSetupForm extends Component {
 
   render() {
     return (
-      <div>
+      <div className='login-container'>
         <form>
-          <h2>Define your Goals</h2>
+          <h2>Define your #Goals</h2>
           <FormControl>
             <InputLabel>Age</InputLabel>
             <Select value={this.state.goalType} onChange={this.newGoalType}>
@@ -106,11 +107,11 @@ class GoalSetupForm extends Component {
         </form>
         <p>
           {this.state.unrealistic ? (
-            <div>
-              A daily savings goal of ${this.state.dailySaveGoal}
-              might not be realistic. Modify your goal, or click
-              <button onClick={this.renderFixedInput}>here</button>
-              to continue
+            <div className='login-failed'>
+              A daily savings goal of ${this.state.dailySaveGoal + ' '}
+              might not be realistic. Modify your goal, or click to continue.
+              <UnrealisticGoalSubmitButton onClick={this.renderFixedInput}>Continue</UnrealisticGoalSubmitButton>
+            
             </div>
           ) : null}
         </p>
