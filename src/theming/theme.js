e98d3e2c9125e25896bfa9ffa31215009fd12b93
Shaun Messenger <shaunmessenger@Shauns-MacBook-Air.local>
2018-08-21 14:01:57 -0400

    login page css styling

--- src/theming/theme.js
+++ src/theming/theme.js
@@ -1,18 +1,18 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 export const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#84c887',
-      main: '#66bb6a',
-      dark: '#47824a',
-      contrastText: '#fff',
+      light: "#66bc6a",
+      main: "#66bc6a",
+      dark: "#47824a",
+      contrastText: "#fff"
     },
     secondary: {
-      light: '#84c887',
-      main: '#00acc1',
-      dark: '#47824a',
-      contrastText: '#000',
-    },
-  },
+      light: "#84c887",
+      main: "#00acc1",
+      dark: "#47824a",
+      contrastText: "#000"
+    }
+  }
 });
