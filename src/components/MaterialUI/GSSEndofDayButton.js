56fb8ebf89c02e448e14f96471799c0307917162
Shaun Messenger <shaunmessenger@Shauns-MacBook-Air.local>
2018-08-22 13:00:05 -0400

    S made buttons same length

--- src/components/MaterialUI/GSSEndofDayButton.js
+++ src/components/MaterialUI/GSSEndofDayButton.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
+import "../../App.css";
 
 class EndofDayButton extends Component {
-
   render() {
-    return <Button variant="contained">End Day</Button>;
+    return (
+      <Button style={{ minWidth: "150px" }} variant="contained">
+        End Day
+      </Button>
+    );
   }
 }
 
